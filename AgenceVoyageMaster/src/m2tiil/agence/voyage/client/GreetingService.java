5bf014c5b3a398f3ecd0be657cc5c8c9a7f8a760
Gauko <rom.antoin@gmail.fr>
2014-03-14 15:27:31 +0100

    Ajout des methodes dans le service.

--- AgenceVoyageMaster/src/m2tiil/agence/voyage/client/GreetingService.java
+++ AgenceVoyageMaster/src/m2tiil/agence/voyage/client/GreetingService.java
@@ -1,5 +1,13 @@
 package m2tiil.agence.voyage.client;
 
+import java.util.HashMap;
+import java.util.List;
+
+import m2tiil.agence.voyage.shared.ConnectionException;
+import m2tiil.agence.voyage.shared.bdd.pojo.Offre;
+import m2tiil.agence.voyage.shared.bdd.pojo.Trajet;
+import m2tiil.agence.voyage.shared.bdd.pojo.Type;
+
 import com.google.gwt.user.client.rpc.RemoteService;
 import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
 
@@ -8,5 +16,16 @@ import com.google.gwt.user.client.rpc.RemoteServiceRelativePath;
  */
 @RemoteServiceRelativePath("greet")
 public interface GreetingService extends RemoteService {
-	String greetServer(String name) throws IllegalArgumentException;
+	
+	public String greetServer(String input) throws IllegalArgumentException;
+	public String login(String userMail, String password) throws ConnectionException;
+	public void registerNewUser(String nom, String prenom, String password, String mail) throws Exception;
+	public List<Type> getTypeTransport(String token) throws ConnectionException;
+	public HashMap<String,List<String>> getCritere(String token) throws ConnectionException;
+	public List<Offre> getOffreDuJour(String token) throws ConnectionException;
+	public List<Trajet> rechercheTrajet(String token, HashMap<String,String> critereValeur) throws ConnectionException;
+	public Object doReservation(String token, String user, Object panier, Object carteBanquaire) throws ConnectionException;
+
+	
+	
 }
--- AgenceVoyageMaster/src/m2tiil/agence/voyage/client/GreetingServiceAsync.java
+++ AgenceVoyageMaster/src/m2tiil/agence/voyage/client/GreetingServiceAsync.java
@@ -1,5 +1,12 @@
 package m2tiil.agence.voyage.client;
 
+import java.util.HashMap;
+import java.util.List;
+
+import m2tiil.agence.voyage.shared.bdd.pojo.Offre;
+import m2tiil.agence.voyage.shared.bdd.pojo.Trajet;
+import m2tiil.agence.voyage.shared.bdd.pojo.Type;
+
 import com.google.gwt.user.client.rpc.AsyncCallback;
 
 /**
@@ -8,4 +15,22 @@ import com.google.gwt.user.client.rpc.AsyncCallback;
 public interface GreetingServiceAsync {
 	void greetServer(String input, AsyncCallback<String> callback)
 			throws IllegalArgumentException;
+
+	void login(String userMail, String password, AsyncCallback<String> callback);
+
+	void registerNewUser(String nom, String prenom, String password,
+			String mail, AsyncCallback<Void> callback);
+
+	void getTypeTransport(String token, AsyncCallback<List<Type>> callback);
+
+	void getCritere(String token,
+			AsyncCallback<HashMap<String, List<String>>> callback);
+
+	void getOffreDuJour(String token, AsyncCallback<List<Offre>> callback);
+
+	void rechercheTrajet(String token, HashMap<String, String> critereValeur,
+			AsyncCallback<List<Trajet>> callback);
+
+	void doReservation(String token, String user, Object panier,
+			Object carteBanquaire, AsyncCallback<Object> callback);
 }
